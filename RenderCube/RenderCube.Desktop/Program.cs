0e09cea898a1beddf59fb14dcffc1860525b23e8
Tristan MacCrehan <Tristan MacCrehan>
2017-05-23 10:24:29 -0400

    Add project files.

---
+++ RenderCube/RenderCube.Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿using Urho;
+
+namespace RenderCube.Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MyGame(new ApplicationOptions("Data")).Run();
+        }
+    }
+}
